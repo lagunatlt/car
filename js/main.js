c61e29f8f8bc2b12cdf726eb4ebae7b2ee29dc5e
lagunatlt <lagunatlt@yandex.ru>
2020-02-27 00:31:04 +0400

    ok

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/* --------tabs-------------- */
+let tab1 = document.getElementById('tab1');
+let tab2 = document.getElementById('tab2');
+let tab3 = document.getElementById('tab3');
+let labelTab1 = document.getElementById('labelTab1');
+let labelTab2 = document.getElementById('labelTab2');
+let labelTab3 = document.getElementById('labelTab3');
+
+let tabFun = function() {
+	if (tab1.checked) {
+		labelTab1.style.borderRight = 'none';
+		labelTab2.style.borderRight = '1px solid #000';
+	}
+	if (tab2.checked) {
+		labelTab1.style.borderRight = 'none';
+		labelTab2.style.borderRight = 'none';
+	}
+	if (tab3.checked) {
+		labelTab1.style.borderRight = '1px solid #000';
+		labelTab2.style.borderRight = 'none';
+	}
+}
+tabFun();
+
+tab1.addEventListener('change', tabFun);
+tab2.addEventListener('change', tabFun);
+tab3.addEventListener('change', tabFun);
+/* --------tabs end-------------- */

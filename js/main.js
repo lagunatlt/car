19112de5a5ace34a5b07f879d9171363808a5a6c
lagunatlt <lagunatlt@yandex.ru>
2020-03-01 20:34:35 +0400

    ok

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+window.onload = function () {
+	document.querySelector('body').style.opacity = '1'
+};
+$(function () {
+	$('.nav-top__item').click(function () {
+		$('body,html').animate({ scrollTop: $('#' + $(this).data('value')).offset().top }, 1000)
+	})
+});
 /* --------tabs-------------- */
 let tab1 = document.getElementById('tab1');
 let tab2 = document.getElementById('tab2');
@@ -25,3 +33,24 @@ tab1.addEventListener('change', tabFun);
 tab2.addEventListener('change', tabFun);
 tab3.addEventListener('change', tabFun);
 /* --------tabs end-------------- */
+
+// let secret1 = document.getElementById('secret1');
+// let secret2 = document.getElementById('secret2');
+// secret1.addEventListener('click', function() {
+// 	secret2.classList.toggle('secret-hide');
+// })
+let accordions = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < accordions.length; i++) {
+	accordions[i].onclick = function () {
+		this.classList.toggle('is-open');
+		this.classList.toggle('rotate');
+
+		let content = this.nextElementSibling;
+		if (content.style.maxHeight) {
+			content.style.maxHeight = null;
+		} else {
+			content.style.maxHeight = content.scrollHeight + "px";
+		}
+	}
+}
